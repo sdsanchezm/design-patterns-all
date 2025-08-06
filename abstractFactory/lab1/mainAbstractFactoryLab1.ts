41f7ab50201e11c307e34edd60b370e19b8bfad7
sdsanchezm <sdsanchezm@gmail.com>
2025-08-05 22:36:53 -0500

    updated: abstractFactory pattern lab1

---
+++ abstractFactory/lab1/mainAbstractFactoryLab1.ts
@@ -0,0 +1,75 @@
+interface Button {
+    render(): void;
+}
+
+interface Checkbox {
+    render(): void;
+}
+
+class WindowsButton implements Button {
+    render(): void {
+        console.log("Rendering a Windows button.");
+    }
+}
+
+class MacOSButton implements Button {
+    render(): void {
+        console.log("Rendering a macOS button.");
+    }
+}
+
+class WindowsCheckbox implements Checkbox {
+    render(): void {
+        console.log("Rendering a Windows checkbox.");
+    }
+}
+
+class MacOSCheckbox implements Checkbox {
+    render(): void {
+        console.log("Rendering a macOS checkbox.");
+    }
+}
+
+interface GUIFactory {
+    createButton(): Button;
+    createCheckbox(): Checkbox;
+}
+
+class WindowsFactory implements GUIFactory {
+    createButton(): Button {
+        return new WindowsButton();
+    }
+
+    createCheckbox(): Checkbox {
+        return new WindowsCheckbox();
+    }
+}
+
+class MacOSFactory implements GUIFactory {
+    createButton(): Button {
+        return new MacOSButton();
+    }
+
+    createCheckbox(): Checkbox {
+        return new MacOSCheckbox();
+    }
+}
+
+function clientCodeAbstractFactoryLab2(factory: GUIFactory) {
+    const button = factory.createButton();
+    const checkbox = factory.createCheckbox();
+
+    button.render();
+    checkbox.render();
+}
+
+function mainAbstractFactoryLab1() {
+    const windowsFactory = new WindowsFactory();
+    clientCodeAbstractFactoryLab2(windowsFactory);
+
+    const macOSFactory = new MacOSFactory();
+    clientCodeAbstractFactoryLab2(macOSFactory);
+}
+
+// run with: deno run --unstable-sloppy-imports mainAbstractFactoryLab1.ts
+mainAbstractFactoryLab1();
