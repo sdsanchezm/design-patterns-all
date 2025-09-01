7053d7bb1bbe18b174fcbbe1ba7720de6211d076
sdsanchezm <sdsanchezm@gmail.com>
2025-08-31 21:42:43 -0500

    added dotnet labs and organized folders

---
+++ Dotnet/Creationals/SingletonLab1/Program.cs
@@ -0,0 +1,21 @@
+﻿using SingletonLab1.UtilityClasses;
+using System;
+
+namespace SingletonLab1;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Start==");
+
+        var a = DatabaseConnection.GetInstance();
+        a.GetCallsOfInstance();
+
+        var b = DatabaseConnection.GetInstance();
+        b.GetCallsOfInstance();
+        
+        var c = DatabaseConnection.GetInstance();
+        c.GetCallsOfInstance();
+        
+    }
+}
---
+++ Dotnet/Creationals/SingletonLab1/UtilityClasses/DatabaseConnection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonLab1.UtilityClasses;
+public class DatabaseConnection
+{
+    private static DatabaseConnection instanceObj = null;
+    private static int numberOfCalls = 0;
+    private DatabaseConnection()
+    {
+        //instance = new DatabaseConnection();
+    }
+
+    public static DatabaseConnection GetInstance()
+    {
+        numberOfCalls++;
+        Console.WriteLine(numberOfCalls);
+        if (instanceObj == null)
+        {
+            instanceObj = new DatabaseConnection();
+        }
+        
+        return instanceObj;
+    }
+
+    public void GetCallsOfInstance()
+    {
+        Console.WriteLine($"calls: {numberOfCalls}");
+    }
+}
